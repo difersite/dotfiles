dd2f2fbd74d4d3c9281295251b97df329872d6c8
difersite <dferruz@difersite.com>
2021-08-16 13:22:23 -0400

    add file via upload 210816 - estandar linux
    Incluyo los atajos que estoy usando como standar en linux, debo estudiar mas de la carpeta funciones y de las funciones en si que no estoy manejando.

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,16 @@
 #
 
 # Config
+set fish_greeting "Bienvenido a Fish"
+set -gx TERM xterm-256color
 set EDITOR "nvim"
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
 
 # Inicio de programas
 neofetch
@@ -14,8 +23,17 @@ function line-count
     wc -l | string trim
 end
 
+function cnt
+  seq 15
+end 
 
 ### Abreviaciones ###
 abbr df 'df -h'
 abbr .. 'cd ..'
-abbr home 'cd ~'
+abbr home 'cd ~dferruz'
+abbr tmp 'cd ~/Z5-Temp'
+abbr cfg 'cd ~/.config'
+abbr doc 'cd ~/D1-Document'
+abbr ll 'exa -l --icons --tree --level=1'
+abbr la 'exa -la --icons --tree --level=1 | sort' ## | awk '{print NR " " $0}'i
+command -qv nvim && alias vim nvim
